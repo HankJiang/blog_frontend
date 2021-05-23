7196970c5ae2fb8a10c91dc43fff7385dd083f66
HankJiang <jianghan.ah@foxmail.com>
2021-05-23 20:41:56 +0800

    update

--- src/requests/BlogRequests.js
+++ src/requests/BlogRequests.js
@@ -4,7 +4,7 @@ import axios from "axios";
 let apiBaseUrl =  "http://api.gsxxm.com";
 
 // let config = { withCredentials: true, crossdomain: true };
-let config = { crossdomain: true };
+let config = { };
 
 export function regist(payload) {
     return axios.post(apiBaseUrl+'/register', payload, config)
